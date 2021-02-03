54149d52d1df612c9bab1eb29b81a9879f4fd3ea
giiaga <ghousali99@gmail.com>
2021-02-03 18:25:58 -0500

    movE

--- baras/src/store/session.js
+++ baras/src/store/session.js
@@ -61,6 +61,14 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const logout = () => async (dispatch) => {
+  const response = await fetch("/api/session", {
+    method: "DELETE",
+  });
+  dispatch(removeUser());
+  return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
