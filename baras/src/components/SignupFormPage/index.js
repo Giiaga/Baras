e13bd268b544550e996df0740519d55be6179cc8
giiaga <ghousali99@gmail.com>
2021-02-03 18:15:06 -0500

    signup component

---
+++ baras/src/components/SignupFormPage/index.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+
+function SignupFormPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector((state) => state.session.user);
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  let [photo, setPhoto] = useState();
+  let [quote, setQuote] = useState();
+  let [description, setDescription] = useState();
+  let [privateAccount, setPrivateAccount] = useState(false);
+
+  const [errors, setErrors] = useState([]);
+
+  if (sessionUser) return <Redirect to="/" />;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password === confirmPassword) {
+      setErrors([]);
+      return dispatch(
+        sessionActions.signup({ email, username, password })
+      ).catch((res) => {
+        if (res.data && res.data.errors) setErrors(res.data.errors);
+      });
+    }
+    return setErrors([
+      "Confirm Password field must be the same as the Password field",
+    ]);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <ul>
+        {errors.map((error, idx) => (
+          <li key={idx}>{error}</li>
+        ))}
+      </ul>
+      <label>
+        Username
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Email
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Photo
+        <input
+          type="text"
+          value={photo}
+          onChange={(e) => setPhoto(e.target.value)}
+        />
+      </label>
+      <label>
+        Quote
+        <input
+          type="text"
+          value={quote}
+          onChange={(e) => setQuote(e.target.value)}
+        />
+      </label>
+      <label>
+        Description
+        <input
+          type="text"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </label>
+      <label>
+        Set Account Private
+        <input
+          type="checkbox"
+          name="privateAccount"
+          checked={privateAccount}
+          onChange={(e) => setPrivateAccount(e.target.value)}
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Confirm Password
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+}
+
+export default SignupFormPage;
