67b6c20d8062e3ab90a1223f9875e285c065b0f8
giiaga <ghousali99@gmail.com>
2021-02-04 00:51:43 -0500

    login route valid

--- backendBaras/routes/api/index.js
+++ backendBaras/routes/api/index.js
@@ -3,8 +3,15 @@ const asyncHandler = require("express-async-handler");
 const { setTokenCookie } = require("../../utils/auth.js");
 const { restoreUser } = require("../../utils/auth.js");
 const { requireAuth } = require("../../utils/auth.js");
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
 
 const { User } = require("../../db/models");
+
+// Routes connect
+router.use("/session", sessionRouter);
+router.use("/users", usersRouter);
+
 router.get("/", (req, res) => {
   res.send("hiiihdsf");
 });
