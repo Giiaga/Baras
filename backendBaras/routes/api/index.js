2c9124851a1a3fae32030ce850b37e9a30e040fa
giiaga <ghousali99@gmail.com>
2021-02-04 00:44:44 -0500

    auth works

--- backendBaras/routes/api/index.js
+++ backendBaras/routes/api/index.js
@@ -1,22 +1,12 @@
 const router = require("express").Router();
 const asyncHandler = require("express-async-handler");
 const { setTokenCookie } = require("../../utils/auth.js");
+const { restoreUser } = require("../../utils/auth.js");
+const { requireAuth } = require("../../utils/auth.js");
+
 const { User } = require("../../db/models");
 router.get("/", (req, res) => {
   res.send("hiiihdsf");
 });
-router.get(
-  "/set-token-cookie",
-  asyncHandler(async (req, res) => {
-    const user = await User.findOne({
-      where: {
-        username: "demoUser1",
-      },
-    });
-
-    setTokenCookie(res, user);
-    return res.json({ user });
-  })
-);
 
 module.exports = router;
