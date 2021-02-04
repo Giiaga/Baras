cb7ad51c0ebf716db212de926c36f4286f3d242d
giiaga <ghousali99@gmail.com>
2021-02-04 00:58:57 -0500

    signup route accomplished

--- backendBaras/routes/api/users.js
+++ backendBaras/routes/api/users.js
@@ -1,4 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const asyncHandler = require("express-async-handler");
+
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
+// Sign up
+router.post(
+  "",
+  asyncHandler(async (req, res) => {
+    const {
+      email,
+      password,
+      username,
+      photo,
+      quote,
+      description,
+      privateAccount,
+    } = req.body;
+    const user = await User.signup({
+      email,
+      username,
+      password,
+      photo,
+      quote,
+      description,
+      privateAccount,
+    });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  })
+);
 
 module.exports = router;
