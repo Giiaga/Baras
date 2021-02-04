710e071af8bf9a88e2e171b3c5461c3a908880c0
giiaga <ghousali99@gmail.com>
2021-02-03 23:55:10 -0500

    it may have just been a spelling error in the .env that made me restart the process

--- backendBaras/db/models/user.js
+++ backendBaras/db/models/user.js
@@ -1,5 +1,6 @@
 "use strict";
 const { Validator } = require("sequelize");
+const bcrypt = require("bcryptjs");
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
@@ -45,10 +46,69 @@ module.exports = (sequelize, DataTypes) => {
         defaultValue: false,
       },
     },
-    {}
+    {
+      defaultScope: {
+        attributes: {
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"],
+        },
+      },
+      scopes: {
+        currentUser: {
+          attributes: { exclude: ["hashedPassword"] },
+        },
+        loginUser: {
+          attributes: {},
+        },
+      },
+    }
   );
   User.associate = function (models) {
     // associations can be defined here
+  };
+  User.prototype.toSafeObject = function () {
+    const { id, username, email } = this; // context will be the User instance
+    return { id, username, email };
+  };
+  User.prototype.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.hashedPassword.toString());
+  };
+  User.getCurrentUserById = async function (id) {
+    return await User.scope("currentUser").findByPk(id);
+  };
+  User.login = async function ({ credential, password }) {
+    const { Op } = require("sequelize");
+    const user = await User.scope("loginUser").findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential,
+        },
+      },
+    });
+    if (user && user.validatePassword(password)) {
+      return await User.scope("currentUser").findByPk(user.id);
+    }
+  };
+  User.signup = async function ({
+    username,
+    email,
+    password,
+    photo,
+    quote,
+    description,
+    privateAccount,
+  }) {
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+      username,
+      email,
+      hashedPassword,
+      photo,
+      quote,
+      description,
+      privateAccount,
+    });
+    return await User.scope("currentUser").findByPk(user.id);
   };
   return User;
 };
--- backendBaras/routes/api/index.js
+++ backendBaras/routes/api/index.js
@@ -1,6 +1,22 @@
 const router = require("express").Router();
-
-router.post("/test", function (req, res) {
-  res.json({ requestBody: req.body });
+const asyncHandler = require("express-async-handler");
+const { setTokenCookie } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
+router.get("/", (req, res) => {
+  res.send("hiiihdsf");
 });
+router.get(
+  "/set-token-cookie",
+  asyncHandler(async (req, res) => {
+    const user = await User.findOne({
+      where: {
+        username: "demoUser1",
+      },
+    });
+
+    setTokenCookie(res, user);
+    return res.json({ user });
+  })
+);
+
 module.exports = router;
