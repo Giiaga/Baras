7faba50db369e1381354822cef77b4fae65d1563
giiaga <ghousali99@gmail.com>
2021-02-06 01:17:23 -0500

    slide menu almost accomplished

--- baras/src/components/SlideMenu/SlideMenu.js
+++ baras/src/components/SlideMenu/SlideMenu.js
@@ -11,10 +11,14 @@ function SlideMenu() {
     <>
       <div hidden={showMenu} className="menu">
         <div className="menuList">
-          <h4>Me</h4>
-          <h4>World Baras</h4>
-          <h4>Trusted Baras</h4>
-          <h4>Trust</h4>
+          <h4 onClick={() => (window.location.href = "/user")}>Me</h4>
+          <h4 onClick={() => (window.location.href = "/world-baras")}>
+            World Baras
+          </h4>
+          <h4 onClick={() => (window.location.href = "/trust/baras")}>
+            Trusted Baras
+          </h4>
+          <h4 onClick={() => (window.location.href = "/trust")}>Trust</h4>
         </div>
       </div>
       <button
