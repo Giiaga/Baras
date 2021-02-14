9e7f89cb3981f127acbe9e9ab7eb92922fb6b16a
giiaga <ghousali99@gmail.com>
2021-02-14 18:52:26 -0500

    embedding soundcloud: gi

--- baras/src/components/Story/Page.js
+++ baras/src/components/Story/Page.js
@@ -331,6 +331,18 @@ function Page() {
             Sorry, your browser does not support audio being used on here
           </audio>
         )} */}
+
+      <>
+        <iframe
+          // width="100%"
+          // height="166"
+          // scrolling="no"
+          // frameborder="no"
+          // allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/924613324&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+        ></iframe>
+      </>
+
       <div>
         <input
           type="text"
