6638bea2f6ca029687c9b367c0970cdd0835bc6f
giiaga <ghousali99@gmail.com>
2021-02-15 20:07:39 -0500

    fixed bug

--- baras/src/components/Story/Page.js
+++ baras/src/components/Story/Page.js
@@ -32,16 +32,16 @@ function Page() {
   //   allPages.forEach((content)=> {
 
   //   })
-  }
+  // }
   let dispatch = useDispatch();
   console.log(allPages, pageNumber, "ALL PAGES");
   useEffect(
     () =>
       dispatch(getPages(storyTitle)).then((response) => {
+        // setAllPagesAvailable(true)
         setPageNumber(response[response.length - 1].pageNumber);
-        setChangePage(response.length - 1)
-      }
-      ),
+        setChangePage(response.length - 1);
+      }),
     []
   );
   let saveStory = () => {
@@ -597,18 +597,28 @@ function Page() {
         ) : (
           ""
         )}
-        {allPages.length ? <button style={{ position: "absolute", top: "49%", left: "-8.9%" }}
-        onClick={()=> i >= 0 && i < allPages.length - 1 ? setChangePage(i + 1) : {}}>
-          Prev
-        </button>
-: '' }
-       {allPages.length ? <button
-          style={{ position: "absolute", top: "50%", left: "100.3%" }}
-          onClick={() => { i > 0 ? setChangePage(i - 1) : {} }}
-        >
-          Next
-        </button>
-        : '' }
+        {i ? (
+          <button
+            style={{ position: "absolute", top: "49%", left: "-8.9%" }}
+            onClick={() =>
+              i >= 0 && i < allPages.length - 1 ? setChangePage(i + 1) : {}
+            }
+          >
+            Prev
+          </button>
+        ) : (
+          ""
+        )}
+        {i ? (
+          <button
+            style={{ position: "absolute", top: "50%", left: "100.3%" }}
+            onClick={() => (i > 0 ? setChangePage(i - 1) : {})}
+          >
+            Next
+          </button>
+        ) : (
+          ""
+        )}
         <p style={{ position: "absolute", top: "97%", left: "94%" }}>
           {pageNumber}
         </p>
