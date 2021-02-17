e2b8e6506471e3f7f3568e858a921bc79a69d73b
giiaga <ghousali99@gmail.com>
2021-02-17 03:54:47 -0500

    slide put menu is what it is

--- baras/src/components/SlideMenu/SlideMenu.js
+++ baras/src/components/SlideMenu/SlideMenu.js
@@ -9,11 +9,11 @@ function SlideMenu() {
   let dispatch = useDispatch();
   let [showMenu, setShowMenu] = useState(true);
   let history = useHistory();
-  let menuButton;
-  if (showMenu) {
-    menuButton = { left: 0 };
-    // document.getElementById("menu").style.width = 300 + "px";
-  }
+  // let menuButton;
+  // if (showMenu) {
+  //   menuButton = { left: 0 };
+  //   // document.getElementById("menu").style.width = 300 + "px";
+  // }
   return (
     <>
       <div className="menu" id="menu">
@@ -38,17 +38,34 @@ function SlideMenu() {
             Logout
           </h4>
         </div>
+        <a
+          href="javascript:void(0)"
+          className="closeButton"
+          onClick={() => {
+            if ((document.getElementById("menu").style.top = 0 + "%")) {
+              document.getElementById("menu").style.top = -70 + "%";
+            }
+          }}
+        >
+          close
+        </a>
       </div>
       <button
         className="menuButton"
-        style={menuButton}
+        id="menuButton"
+        // style={menuButton}
         onClick={() => {
-          showMenu ? setShowMenu(false) : setShowMenu(true);
-          if (document.getElementById("menu").style.top === 0) {
-            document.getElementById("menu").style.top = 15 + "%";
-          } else {
+          // showMenu ? setShowMenu(false) : setShowMenu(true);
+          // document.getElementById("menuButton").style.top === 2 + "%"
+          //   ? (document.getElementById("menuButton").style.top = 15 + "%")
+          //   : (document.getElementById("menuButton").style.top = 2 + "%");
+          if (document.getElementById("menu").style.top === -70 + "%") {
             document.getElementById("menu").style.top = 0 + "%";
           }
+          // } else if ((document.getElementById("menu").style.top = 0 + "%")) {
+          //   document.getElementById("menu").style.top = -70 + "%";
+          //   document.getElementById("menuButton").style.top = 0 + "%";
+          // }
         }}
       >
         Show
