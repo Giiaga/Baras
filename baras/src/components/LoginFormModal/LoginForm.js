8726181b494352a34be9f6ffee5fff3c48e914b0
giiaga <ghousali99@gmail.com>
2021-02-19 03:57:49 -0500

    Login page added more messages

--- baras/src/components/LoginFormModal/LoginForm.js
+++ baras/src/components/LoginFormModal/LoginForm.js
@@ -163,6 +163,538 @@ function LoginForm() {
                 You are Life
               </p>
             </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "120%",
+                top: "140%",
+                width: "200px",
+                transform: "rotate(-10deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Be You!
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "-40%",
+                top: "160%",
+                width: "200px",
+                transform: "rotate(90deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Life
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "0%",
+                top: "106%",
+                width: "200px",
+                transform: "rotate(20deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Let it out
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "173%",
+                top: "156%",
+                width: "200px",
+                transform: "rotate(20deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                You desire this
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "173%",
+                top: "116%",
+                width: "200px",
+                transform: "rotate(-12deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Find Yourself
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "173%",
+                top: "196%",
+                width: "200px",
+                transform: "rotate(-52deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Feel It
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "93%",
+                top: "195%",
+                width: "200px",
+                transform: "rotate(0deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Don't fear Yourself
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "300%",
+                top: "170%",
+                width: "200px",
+                transform: "rotate(-120deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                You are Life
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "106%",
+                top: "50%",
+                width: "200px",
+                transform: "rotate(30deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Be So Happy That ..
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "311%",
+                top: "95%",
+                width: "200px",
+                transform: "rotate(40deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Desire
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "411%",
+                top: "-25%",
+                width: "200px",
+                transform: "rotate(40deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                TRULY Desire
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "341%",
+                top: "49%",
+                width: "200px",
+                transform: "rotate(-5deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                TRULY Desire
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "283%",
+                top: "58%",
+                width: "200px",
+                transform: "rotate(7deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Let Yourself
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "283%",
+                top: "200%",
+                width: "200px",
+                transform: "rotate(7deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Be You!
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "213%",
+                top: "200%",
+                width: "200px",
+                transform: "rotate(-7deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Love
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "374%",
+                top: "-23%",
+                width: "150px",
+                transform: "rotate(12deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "190px",
+                  fontStyle: "italic",
+                }}
+              >
+                You really are Great. See it!
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "215%",
+                top: "-30%",
+                width: "200px",
+                transform: "rotate(0deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Find Yourself
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "125%",
+                top: "-30%",
+                width: "200px",
+                transform: "rotate(10deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                You can do it
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "-25%",
+                top: "-30%",
+                width: "200px",
+                transform: "rotate(10deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Life is You
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "-25%",
+                top: "190%",
+                width: "200px",
+                transform: "rotate(-35deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Enjoy Every Moment
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "405%",
+                top: "25%",
+                width: "200px",
+                transform: "rotate(88deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Be So Happy
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "282%",
+                top: "116%",
+                width: "200px",
+                transform: "rotate(-3deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                If You will
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "250%",
+                top: "156%",
+                width: "200px",
+                transform: "rotate(-1deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Express
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "233%",
+                top: "105%",
+                width: "200px",
+                transform: "rotate(70deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Let it out
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "300%",
+                top: "20%",
+                width: "200px",
+                transform: "rotate(-10deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Be You!
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "50%",
+                top: "130%",
+                width: "200px",
+                transform: "rotate(-10deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Understand Yourself
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "350%",
+                top: "20%",
+                width: "200px",
+                transform: "rotate(10deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Do it!
+              </p>
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                left: "50%",
+                top: "180%",
+                width: "200px",
+                transform: "rotate(10deg)",
+              }}
+            >
+              <p
+                style={{
+                  fontSize: "40px",
+                  width: "340px",
+                  fontStyle: "italic",
+                }}
+              >
+                Do it!
+              </p>
+            </div>
             <span
               id="tets"
               type="button"
