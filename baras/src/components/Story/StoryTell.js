d8b6f63d844f3d3d5a26b88a4265ee4f380996f9
giiaga <ghousali99@gmail.com>
2021-02-22 19:17:21 -0500

    Process

--- baras/src/components/Story/StoryTell.js
+++ baras/src/components/Story/StoryTell.js
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { tellStory } from "../../store/story";
+import "./StoryTell.css";
 
 function StoryTell() {
   let [publish, setPublished] = useState(false);
+  let [photo, setPhoto] = useState("");
   let [title, setTitle] = useState("");
+
   let userId = useSelector((state) => state.session.user.id);
   let dispatch = useDispatch();
   let history = useHistory();
+
   let createStory = (e) => {
     e.preventDefault();
     dispatch(tellStory(userId, title, publish)).then((data) => {
@@ -16,18 +20,30 @@ function StoryTell() {
       if (data) {
         return history.push(`/story/${paramTitle}/cont`);
       }
-      // return history.push(`/story/${paramTitle}/cont`);
     });
   };
+
+  let changePhoto = (data) => {
+    let fileread = new FileReader();
+    fileread.onload = (e) => {
+      setPhoto(e.target.result);
+    };
+    fileread.readAsDataURL(data);
+  };
+
   return (
     <>
-      <form onSubmit={createStory}>
+      <form onSubmit={createStory} className="storyTellForm">
         <input
           type="text"
           value={title}
           placeholder="Title"
           onChange={(e) => setTitle(e.target.value)}
         />
+        <label>
+          Photo
+          <input type="file" onChange={(e) => changePhoto(e.target.files[0])} />
+        </label>
         <label>
           Publish{" "}
           <input
@@ -38,7 +54,26 @@ function StoryTell() {
             }
           />
         </label>
-        <button>Start</button>
+        {photo && (
+          <div
+            style={{
+              border: "1px solid orange",
+              maxHeight: "600px",
+              maxWidth: "600px",
+              height: "300px",
+              width: "300px",
+              minHeight: "100px",
+              minWidth: "100px",
+            }}
+          >
+            <img
+              src={photo}
+              alt="temp"
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
+        )}
+        <button type="submit">Start</button>
       </form>
     </>
   );
