277b9ff2120a1500ef993badcecb1feb6bea6629
giiaga <ghousali99@gmail.com>
2021-02-25 13:37:00 -0500

    Thoughts done

--- baras/src/components/Thoughts/renderThoughtsModal.js
+++ baras/src/components/Thoughts/renderThoughtsModal.js
@@ -32,8 +32,60 @@ function renderThoughts(allThoughts) {
         );
       }
     }
+    if (thought.video) {
+      styledThoughts.push(
+        <div className="mainThoughtDiv">
+          <div className="thoughtUsernameImageDiv">
+            <div className="thoughtUserImage">
+              <img src={thought.User.photo} alt={thought.User.username} />
+            </div>
+            <NavLink
+              to={`/${thought.User.username}/user`}
+              className="thoughtUsername"
+            >
+              {thought.User.username}
+            </NavLink>
+          </div>
+          <div className="thoughtImageDiv">
+            <iframe src={thought.video}></iframe>
+          </div>
+          <div className="thoughtWithImageText">
+            <p>{thought.text}</p>
+          </div>
+          <div className="thoughtWithImageUpdatedAt">
+            <span>thought: {thought.updatedAt.slice(11, 19)}</span>
+          </div>
+        </div>
+      );
+    }
+    if (thought.music) {
+      styledThoughts.push(
+        <div className="mainThoughtDiv">
+          <div className="thoughtUsernameImageDiv">
+            <div className="thoughtUserImage">
+              <img src={thought.User.photo} alt={thought.User.username} />
+            </div>
+            <NavLink
+              to={`/${thought.User.username}/user`}
+              className="thoughtUsername"
+            >
+              {thought.User.username}
+            </NavLink>
+          </div>
+          <div className="thoughtImageDiv">
+            <iframe src={thought.music}></iframe>
+          </div>
+          <div className="thoughtWithImageText">
+            <p>{thought.text}</p>
+          </div>
+          <div className="thoughtWithImageUpdatedAt">
+            <span>thought: {thought.updatedAt.slice(11, 19)}</span>
+          </div>
+        </div>
+      );
+    }
 
-    if (thought.text) {
+    if (thought.text && !thought.music && !thought.video && !thought.photo) {
       styledThoughts.push(
         <div className="mainThoughtDiv">
           <div className="thoughtUsernameImageDiv">
