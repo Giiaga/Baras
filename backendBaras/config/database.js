bf640c23d3afe275d15ab2a04a3e7ddf389e502b
giiaga <ghousali99@gmail.com>
2021-02-25 16:08:19 -0500

    config

--- backendBaras/config/database.js
+++ backendBaras/config/database.js
@@ -17,6 +17,7 @@ module.exports = {
   },
   production: {
     use_env_variable: "DATABASE_URL",
+    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
     dialect: "postgres",
     seederStorage: "sequelize",
   },
