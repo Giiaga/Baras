139ec81a098312f869f6fbda3a05576e578e1370
giiaga <ghousali99@gmail.com>
2021-02-25 12:26:56 -0500

    thought with only text added

--- baras/src/components/Thoughts/renderThoughtsModal.js
+++ baras/src/components/Thoughts/renderThoughtsModal.js
@@ -32,6 +32,30 @@ function renderThoughts(allThoughts) {
         );
       }
     }
+
+    if (thought.text) {
+      styledThoughts.push(
+        <div className="mainThoughtDiv">
+          <div className="thoughtUsernameImageDiv">
+            <div className="thoughtUserImage">
+              <img src={thought.User.photo} alt={thought.User.username} />
+            </div>
+            <NavLink
+              to={`/${thought.User.username}/user`}
+              className="thoughtUsername"
+            >
+              {thought.User.username}
+            </NavLink>
+          </div>
+          <div className="thoughtWithImageText">
+            <p>{thought.text}</p>
+          </div>
+          <div className="thoughtWithImageUpdatedAt">
+            <span>thought: {thought.updatedAt.slice(11, 19)}</span>
+          </div>
+        </div>
+      );
+    }
   });
   return styledThoughts;
 }
