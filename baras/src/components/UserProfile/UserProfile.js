c4685ef907392cc1d32f66372234de503b14a348
giiaga <ghousali99@gmail.com>
2021-02-25 21:51:16 -0500

    user profile not done

--- baras/src/components/UserProfile/UserProfile.js
+++ baras/src/components/UserProfile/UserProfile.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { getUser, allBaras } from "../../store/user";
+import renderUserBaras from "./renderUserBaras";
 import "./UserProfile.css";
 
 function UserProfile() {
@@ -32,19 +33,31 @@ function UserProfile() {
               <img src={user.photo} alt={user.username} className="userPhoto" />
             </div>
             <div className="userNameDiv">
-              <h1>{user.username}</h1>
+              <h1 style={{ marginTop: "8px" }}>{user.username}</h1>
             </div>
             <div className="userNameDiv">
-              <p style={{ fontStyle: "italic" }}>'{user.quote}'</p>
+              <p
+                style={{
+                  fontStyle: "italic",
+                  color: "rgb(210, 147, 68, 0.9)",
+                  marginTop: "0",
+                  marginBottom: "0",
+                }}
+              >
+                '{user.quote}'
+              </p>
             </div>
-            <div className="userNameDiv">
+            <div
+              className="userNameDiv"
+              style={{ color: "rgb(210, 147, 68, 0.9)", marginTop: "0" }}
+            >
               <p>{user.description}</p>
             </div>
           </div>
           {barasAvailable ? (
-            <div className="allBarasDiv">
-              {userBaras[userBaras.length - 1].relatesTo}
-            </div>
+            renderUserBaras(userBaras).map((each) => (
+              <div className="allBarasDiv">{each[1]}</div>
+            ))
           ) : (
             <div className="allBarasDiv">No Baras</div>
           )}
---
+++ baras/src/components/UserProfile/renderUserBaras.js
@@ -0,0 +1,225 @@
+// import { NavLink } from "react-router-dom";
+
+function renderUserBaras(worldBaras) {
+  let styledWorldBaras = [];
+  worldBaras.map((eachBaras) => {
+    if (!eachBaras.photo && !eachBaras.music && !eachBaras.video) {
+      if (eachBaras.text) {
+        styledWorldBaras.push([
+          eachBaras,
+          <div className="onlyTextBarasDiv">
+            <div className="relatesToTextOnlyDiv">
+              <h3>{eachBaras.relatesTo}</h3>
+            </div>
+            <div className="textTextOnlyDiv">
+              <p>{eachBaras.text}</p>
+            </div>
+            {/* <div className="usernameTextOnlyDiv">
+              <span>by</span>{" "}
+              <NavLink
+                style={{ color: "rgb(131, 129, 125)" }}
+                className="navlink"
+                style={{ color: "rgb(131, 129, 125)" }}
+                to={`/${eachBaras.User.username}/user`}
+              >
+                {eachBaras.User.username}
+              </NavLink>
+            </div> */}
+            <div className="createdAtTextOnlyDiv">
+              <span>let out: {eachBaras.updatedAt.slice(0, 10)}</span>
+            </div>
+          </div>,
+        ]);
+      } else {
+        styledWorldBaras.push([
+          eachBaras,
+          <div className="onlyTextBarasDiv">
+            <div className="relatesToTextOnlyDiv">
+              <h3>{eachBaras.relatesTo}</h3>
+            </div>
+            <div className="textTextOnlyDiv"></div>
+            {/* <div className="usernameTextOnlyDiv">
+              <span>by</span>{" "}
+              <NavLink
+                style={{ color: "rgb(131, 129, 125)" }}
+                to={`/${eachBaras.User.username}/user`}
+              >
+                {eachBaras.User.username}
+              </NavLink>
+            </div> */}
+            <div className="createdAtTextOnlyDiv">
+              <span>let out: {eachBaras.updatedAt.slice(0, 10)}</span>
+            </div>
+          </div>,
+        ]);
+      }
+    }
+
+    if (eachBaras.photo) {
+      if (eachBaras.text) {
+        styledWorldBaras.push([
+          eachBaras,
+          <div className="modalMusicBarasDiv">
+            <div className="modalRelatesToMusicDiv">
+              <h3>{eachBaras.relatesTo}</h3>
+            </div>
+            <div className="modalMusicMusicDiv">
+              <img src={eachBaras.photo} alt={eachBaras.relatesTo} />
+            </div>
+            <div className="modalTextMusicDiv">
+              <p>{eachBaras.text}</p>
+            </div>
+            {/* <div className="usernamePhotoDiv">
+              <span>by</span>{" "}
+              <NavLink
+                style={{ color: "rgb(131, 129, 125)" }}
+                to={`/${eachBaras.User.username}/user`}
+              >
+                {eachBaras.User.username}
+              </NavLink>
+            </div> */}
+            <div className="createdAtPhotoDiv">
+              <span>let out: {eachBaras.updatedAt.slice(0, 10)}</span>
+            </div>
+          </div>,
+        ]);
+      } else {
+        styledWorldBaras.push([
+          eachBaras,
+          <div className="photoBarasDiv">
+            <div className="relatesToPhotoDiv">
+              <h3>{eachBaras.relatesTo}</h3>
+            </div>
+            <div className="photoPhotoDiv">
+              <img src={eachBaras.photo} alt={eachBaras.relatesTo} />
+            </div>
+            {/* <div className="usernamePhotoDiv">
+              <span>by</span>{" "}
+              <NavLink
+                style={{ color: "rgb(131, 129, 125)" }}
+                to={`/${eachBaras.User.username}/user`}
+              >
+                {eachBaras.User.username}
+              </NavLink>
+            </div> */}
+            <div className="createdAtPhotoDiv">
+              <span>let out: {eachBaras.updatedAt.slice(0, 10)}</span>
+            </div>
+          </div>,
+        ]);
+      }
+    }
+    if (eachBaras.video) {
+      if (eachBaras.text) {
+        styledWorldBaras.push([
+          eachBaras,
+          <div className="modalMusicBarasDiv">
+            <div className="modalRelatesToMusicDiv">
+              <h3>{eachBaras.relatesTo}</h3>
+            </div>
+            <div className="modalMusicMusicDiv">
+              <iframe src={eachBaras.video}></iframe>
+            </div>
+            <div className="modalTextMusicDiv">
+              <p>{eachBaras.text}</p>
+            </div>
+            {/* <div className="usernameVideoDiv">
+              <span>by</span>{" "}
+              <NavLink
+                style={{ color: "rgb(131, 129, 125)" }}
+                to={`/${eachBaras.User.username}/user`}
+              >
+                {eachBaras.User.username}
+              </NavLink>
+            </div> */}
+            <div className="createdAtVideoDiv">
+              <span>let out: {eachBaras.updatedAt.slice(0, 10)}</span>
+            </div>
+          </div>,
+        ]);
+      } else {
+        styledWorldBaras.push([
+          eachBaras,
+          <div className="videoBarasDiv">
+            <div className="relatesToVideoDiv">
+              <h3>{eachBaras.relatesTo}</h3>
+            </div>
+            <div className="videoVideoDiv">
+              <iframe src={eachBaras.video}></iframe>
+            </div>
+            {/* <div className="usernameVideoDiv">
+              <span>by</span>{" "}
+              <NavLink
+                style={{ color: "rgb(131, 129, 125)" }}
+                to={`/${eachBaras.User.username}/user`}
+              >
+                {eachBaras.User.username}
+              </NavLink>
+            </div> */}
+            <div className="createdAtVideoDiv">
+              <span>let out: {eachBaras.updatedAt.slice(0, 10)}</span>
+            </div>
+          </div>,
+        ]);
+      }
+    }
+    if (eachBaras.music) {
+      if (eachBaras.text) {
+        styledWorldBaras.push([
+          eachBaras,
+          <div className="modalMusicBarasDiv">
+            <div className="modalRelatesToMusicDiv">
+              <h3>{eachBaras.relatesTo}</h3>
+            </div>
+            <div className="modalMusicMusicDiv">
+              <iframe src={eachBaras.music}></iframe>
+            </div>
+            <div className="modalTextMusicDiv">
+              <p>{eachBaras.text}</p>
+            </div>
+            {/* <div className="usernameMusicDiv">
+              <span>by</span>{" "}
+              <NavLink
+                style={{ color: "rgb(131, 129, 125)" }}
+                to={`/${eachBaras.User.username}/user`}
+              >
+                {eachBaras.User.username}
+              </NavLink>
+            </div> */}
+            <div className="createdAtMusicDiv">
+              <span>let out: {eachBaras.updatedAt.slice(0, 16)}</span>
+            </div>
+          </div>,
+        ]);
+      }
+      //   } else {
+      // styledWorldBaras.push([
+      //   eachBaras,
+      //   <div className="musicBarasDiv">
+      //     <div className="relatesToMusicDiv">
+      //       <h3>{eachBaras.relatesTo}</h3>
+      //     </div>
+      //     <div className="musicMusicDiv">
+      //       <iframe src={eachBaras.music}></iframe>
+      //     </div>
+      //     <div className="usernameMusicDiv">
+      //       <span>by</span>{" "}
+      //       <NavLink
+      //         style={{ color: "rgb(131, 129, 125)" }}
+      //         to={`/${eachBaras.User.username}/user`}
+      //       >
+      //         {eachBaras.User.username}
+      //       </NavLink>
+      //     </div>
+      //     <div className="createdAtMusicDiv">
+      //       <span>let out: {eachBaras.updatedAt.slice(0, 16)}</span>
+      //     </div>
+      //   </div>,
+      //     ]);
+      //   }
+    }
+  });
+  return styledWorldBaras;
+}
+
+export default renderUserBaras;
